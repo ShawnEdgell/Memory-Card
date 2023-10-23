2fe252056759759e76abcf318bc38e759416af46
shawn edgell <shawn_edgell@hotmail.com>
2023-10-23 06:12:23 -0400

    modularized App into components

---
+++ src/components/Controls.svelte
@@ -0,0 +1,22 @@
+<!-- Controls.svelte -->
+
+<script lang="ts">
+    export let difficulty: string;
+    export let handleChange: (e: Event) => void;
+  </script>
+  
+  <div class="controls">
+    <select bind:value={difficulty} on:change={handleChange}>
+      <option value="easy">Easy</option>
+      <option value="medium">Medium</option>
+      <option value="hard">Hard</option>
+    </select>
+  </div>
+  
+  <style>
+    /* Styles for the Controls component go here */
+    .controls {
+      margin-bottom: 20px;
+    }
+  </style>
+  
---
+++ src/components/Scores.svelte
@@ -0,0 +1,24 @@
+<!-- Scores.svelte -->
+<script lang="ts">
+    export let currentScore: number;
+    export let bestScore: number;
+    export let moves: number;
+    export let maxMoves: number;
+  </script>
+  
+  <div class="scores">
+    <p>Current Score: {currentScore}</p>
+    <p>Best Score: {bestScore}</p>
+    <p>Move: {moves} / {maxMoves}</p>
+  </div>
+  
+  <style>
+    /* Your styles for the Scores component */
+    .scores {
+      display: flex;
+      align-items: center;
+      gap: 20px;
+      margin-bottom: 20px;
+    }
+  </style>
+  
